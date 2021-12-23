f0ab1f58fc4472a0b17838307dd237ea845a52a3
Busra Ozcan <ozcan.busra1999@gmail.com>
2021-12-23 18:13:56 +0300

    static files added

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const ejs = require("ejs");
+
+app.set('view engine', "ejs");
+
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description",
-  };
-  res.send(blog);
+  res.render("index")
+});
+app.get("/about", (req, res) => {
+  res.render("about")
+});
+app.get("/addpost", (req, res) => {
+  res.render("add_post")
+});
+app.get("/post", (req, res) => {
+  res.render("post")
 });
 
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
